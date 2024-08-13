916b5437275da78e16022155216cb3f0c1599b40
Faysel Abdella <fayselcode@gmail.com>
2024-08-13 16:56:02 -0400

    Update 8

--- src/pages/Contents.tsx
+++ src/pages/Contents.tsx
@@ -5,7 +5,7 @@ import assets from "../assets/index";
 const Contents = () => {
   const [selectedType, setSelectedType] = useState("all");
   return (
-    <section className="max-container pt-7 md:pt-10 px-3">
+    <section className="max-container pt-7 md:pt-10 ">
       <section>
         <header>
           <div className="flex items-center justify-center gap-32 md:gap-48 ">
@@ -14,7 +14,7 @@ const Contents = () => {
               Explore content
             </h2>
           </div>
-          <div className="max-w-md mx-auto mt-5">
+          <div className="max-w-md mx-auto mt-5 px-3">
             <div className="relative">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <img src={assets.explore} alt="search" className="size-5" />
@@ -29,32 +29,31 @@ const Contents = () => {
             </div>
           </div>
 
-          <aside className="overflow-x-auto w-[700px] max-md:max-w-[100%] mx-auto">
-            <div className="w-full bg-gradient-to-br from-yellow-50 via-cyan-500 to-blue-800 py-2 mt-6 mb-3">
-              <ul className="flex items-center justify-center  ">
+          <aside className="overflow-x-auto w-[700px] max-md:max-w-full mx-auto">
+            <div className="w-full  bg-gradient-to-br from-yellow-50 via-cyan-500 to-blue-800 py-2 mt-6 mb-3 whitespace-nowrap">
+              <ul className="flex items-center md:justify-center  w-full overflow-x-auto px-5">
                 <li className="me-5">
                   <button
                     onClick={() => setSelectedType("all")}
-                    className={`${
-                      selectedType === "all"
-                        ? " text-white border-b-[4px] border-white"
-                        : " text-white opacity-50"
-                    } font-semibold text-[18px] md:text-[24px]`}
+                    className="${selectedType === 'all' ? 'text-white border-b-[4px] border-white' : 'text-white opacity-50'} font-semibold text-[18px] md:text-[24px]"
                   >
                     All
                   </button>
                 </li>
+
                 <li className="me-5">Test</li>
                 <li className="me-5">School</li>
                 <li className="me-5">Life</li>
                 <li className="me-5">Business</li>
                 <li className="me-5">Health</li>
                 <li className="me-5">Food</li>
+                <li className="me-5">Food</li>
+                <li className="me-5">Food</li>
               </ul>
             </div>
           </aside>
         </header>
-        <main></main>
+        <main className="px-3"></main>
       </section>
     </section>
   );
